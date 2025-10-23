e07cc964ad33bae05eba6dcebf507ffb16de9f97
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-23 20:46:36 +0530

    Swap

---
+++ swap.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+print(f"Before swapping: a = {a}, b = {b}")
+
+
+temp = a
+a = b
+b = temp
+
+
+print(f"After swapping: a = {a}, b = {b}")
